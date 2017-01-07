77bebec632517f52a2f6992755313e659d39f4a9
Abinash Maharana <iamabinashmaharana@gmail.com>
2017-01-07 22:44:19 +0530

    Module 5 Solutions.

--- mod5-solution/js/script.js
+++ mod5-solution/js/script.js
@@ -82,11 +82,9 @@ document.addEventListener("DOMContentLoaded", function (event) {
 // On first load, show home view
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
-  homeHtmlUrl,
-  function(responseText){
-    document.querySelector("#main-content").innerHTML = responseText;
-  }, // ***** <---- TODO: STEP 1: Substitute [...] ******
-  false); // Explicitely setting the flag to get JSON from server processed into an object literal
+  allCategoriesUrl,
+  buildAndShowHomeHTML, // ***** <---- TODO: STEP 1: Substitute [...] ******
+  true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
 
@@ -97,17 +95,13 @@ function buildAndShowHomeHTML (categories) {
 
   // Load home snippet page
   $ajaxUtils.sendGetRequest(
-    categoriesTitleHtml,
-    function (categoriesTitleHtml) {
-       $ajaxUtils.sendGetRequest(categoryHtml,
-        function(categoryHtml){
-          var categoriesViewHtml = buildCategoriesViewHtml(categories,categoriesTitleHtml,categoryHtml);
-          insertHtml("#main-content").categoriesViewHtml();
-       },false);
+    homeHtmlUrl,
+    function (homeHtml) {
+
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
-      // var chosenCategoryShortName = ....
+       var chosenCategoryShortName = chooseRandomCategory(categories).short_name; 
 
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
@@ -121,13 +115,15 @@ function buildAndShowHomeHTML (categories) {
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      // var homeHtmlToInsertIntoMainPage = ....
+        chosenCategoryShortName = "'" + chosenCategoryShortName + "'";
+         var homeHtmlToInsertIntoMainPage = insertProperty(homeHtml, "randomCategoryShortName", chosenCategoryShortName);
 
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
       // ....
+       insertHtml("#main-content", homeHtmlToInsertIntoMainPage);      
 
     },
     false); // False here because we are getting just regular HTML from the server, so no need to process JSON.
